80062248aa37b1e55ffa680311e48c2158ccb6ce
ericlam414 <ericL29@nycstudents.net>
2020-10-26 07:57:57 -0400

    code

--- 07-strings/Strings.java
+++ 07-strings/Strings.java
@@ -10,5 +10,13 @@ class Strings{
     word = word.toLowerCase();
     System.out.println("Your word is now: " + word );
 
+    System.out.println("Please enter the characters in the word that you want to replace.");
+    String old = s.nextLine();
+
+    System.out.println("Pleasae enter the characters that you want " + " to be replaced with.");
+    String current = s.nextLine();
+
+    word = word.replaceAll(old, current);
+    System.out.println("Your new word is now: " + word);
   }//end main
 }//end class
