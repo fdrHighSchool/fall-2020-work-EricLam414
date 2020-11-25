e57b932343930002e12a6808a97047cca8548fa9
EricLam414 <71828524+EricLam414@users.noreply.github.com>
2020-11-24 23:03:29 -0500

    Add files via upload

---
+++ 09-FizzBuzz/FizzBuzz.java
@@ -0,0 +1,40 @@
+public class FizzBuzz{
+  public static void main(String[] args) {
+
+    for(int i = 1; i <= 100; i++){
+      //insert logic here to execute FizzBuzz and generate output
+      boolean fizz = isFizz(i);
+      boolean buzz = isBuzz(i);
+      System.out.println(i + " : " + (fizz &&  buzz ? "FizzBuzz" : (fizz ? "Fizz" : (buzz ? "Buzz" : i))));
+    }//end for loop
+
+  }//end main method
+
+  //***********************************************************//
+
+  /*
+   * N: isFizz
+   * P: determine if the number is to be classified as "fizz"
+        a number is classified as "fizz" if it's divisible by 3
+   * I: an integer
+   * R: true/false value
+   */
+  public static boolean isFizz(int num){
+    return (num % 3 == 0);
+
+  }//end isFizz method
+
+  //***********************************************************//
+
+  /*
+   * N: isBuzz
+   * P: determine if the number is to be classified as "buzz"
+        a number is classified as "buzz" if it's divisible by 5
+   * I: an integer
+   * R: true/false value
+   */
+  public static boolean isBuzz(int num){
+    return (num % 5 == 0);
+  }//end isBuzz method
+
+}//end class
